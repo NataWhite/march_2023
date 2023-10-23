3cd2e79aa5c8e80caff3944c2af1227477ffe38c
NataWhite <nata.ivanova.one@gmail.com>
2023-10-23 23:26:22 +0300

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import {UserController} from "./user/user.controller";
-import {UserService} from "./user/user.service";
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [UserModule, AuthModule, UserModule],
-  controllers: [AppController, UserController],
+  controllers: [AppController],
   providers: [AppService, UserService],
 })
 export class AppModule {}
